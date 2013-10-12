0c37de14c065b896be7a73acf19fbb240d77b8f8
kovacsv <kowik1984@gmail.com>
2013-10-12 11:20:54 +0200

    Fix orientation errors in SVG to model converter.

--- src/extras/svgtomodel.js
+++ src/extras/svgtomodel.js
@@ -278,53 +278,68 @@ JSM.SvgToModel = function (svgObject, height, segmentLength)
 	
 	function ContourPolygonToPrisms (polygon, height)
 	{
-		function CreateBasePolygon (polygon)
+		function CreateBasePolygon (polygon, orientation)
 		{
 			var basePolygon = [];
 			
 			var i, coord;
-			for (i = 0; i < polygon.VertexCount (); i++) {
-				coord = polygon.GetVertex (i);
-				basePolygon.push (new JSM.Coord (coord.x, 0.0, -coord.y));
+			if (orientation == 'Clockwise') {
+				for (i = 0; i < polygon.VertexCount (); i++) {
+					coord = polygon.GetVertex (i);
+					basePolygon.push (new JSM.Coord (coord.x, 0.0, -coord.y));
+				}
+			} else {
+				for (i = polygon.VertexCount () - 1; i >= 0; i--) {
+					coord = polygon.GetVertex (i);
+					basePolygon.push (new JSM.Coord (coord.x, 0.0, -coord.y));
+				}
 			}
 
 			return basePolygon;
 		}
 	
-		function AddHoleToBasePolygon (basePolygon, polygon)
+		function AddHoleToBasePolygon (basePolygon, polygon, orientation)
 		{
 			basePolygon.push (null);
 		
 			var i, coord;
-			for (i = 0; i < polygon.VertexCount (); i++) {
-				coord = polygon.GetVertex (i);
-				basePolygon.push (new JSM.Coord (coord.x, 0.0, -coord.y));
+			if (orientation == 'CounterClockwise') {
+				for (i = 0; i < polygon.VertexCount (); i++) {
+					coord = polygon.GetVertex (i);
+					basePolygon.push (new JSM.Coord (coord.x, 0.0, -coord.y));
+				}
+			} else {
+				for (i = polygon.VertexCount () - 1; i >= 0; i--) {
+					coord = polygon.GetVertex (i);
+					basePolygon.push (new JSM.Coord (coord.x, 0.0, -coord.y));
+				}
 			}
 		}
 
 		var prisms = [];
-		var direction = new JSM.Vector (0.0, 1.0, 0.0);
+		var direction = new JSM.Vector (0.0, -1.0, 0.0);
 		
-		var basePolygon, prism;
+		var basePolygon, baseOrientation, prism;
 		var contourCount = polygon.ContourCount ();
 		if (contourCount == 1) {
-			basePolygon = CreateBasePolygon (polygon.GetContour (0));
+			baseOrientation = JSM.PolygonOrientation2D (polygon.GetContour (0));
+			basePolygon = CreateBasePolygon (polygon.GetContour (0), baseOrientation);
 			prism = JSM.GeneratePrism (basePolygon, direction, height, true);
 			prisms.push (prism);
 		} else if (contourCount > 1) {
-			var baseOrientation = JSM.PolygonOrientation2D (polygon.GetContour (0));
-			var holeBasePolygon = CreateBasePolygon (polygon.GetContour (0));
+			baseOrientation = JSM.PolygonOrientation2D (polygon.GetContour (0));
+			var holeBasePolygon = CreateBasePolygon (polygon.GetContour (0), baseOrientation);
 			var hasHoles = false;
 			
 			var i, orientation;
 			for (i = 1; i < polygon.ContourCount (); i++) {
 				orientation = JSM.PolygonOrientation2D (polygon.GetContour (i));
 				if (orientation == baseOrientation) {
-					basePolygon = CreateBasePolygon (polygon.GetContour (i));
+					basePolygon = CreateBasePolygon (polygon.GetContour (i), baseOrientation);
 					prism = JSM.GeneratePrism (basePolygon, direction, height, true);
 					prisms.push (prism);
 				} else {
-					AddHoleToBasePolygon (holeBasePolygon, polygon.GetContour (i));
+					AddHoleToBasePolygon (holeBasePolygon, polygon.GetContour (i), orientation);
 					hasHoles = true;
 				}
 			}
