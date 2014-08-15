a68f9c9576cf6433b86b47acb57fe319aad1eeb6
kovacsv <kowik1984@gmail.com>
2014-08-15 21:20:53 +0200

    Replace three viewer to internal viewer in demonstrations.

--- documentation/olddemo/include/demo.js
+++ documentation/olddemo/include/demo.js
@@ -10,17 +10,17 @@ JSMDemo.prototype =
 {
 	Initialize : function (canvasName, uiDivName)
 	{
-		var viewerSettings = {
-			cameraEyePosition : [-3.0, -2.5, 2.0],
-			cameraCenterPosition : [0.0, 0.0, 0.0],
-			cameraUpVector : [0.0, 0.0, 1.0]
-		};
+		var camera = new JSM.Camera (
+			new JSM.Coord (-3.0, -2.5, 2.0),
+			new JSM.Coord (0.0, 0.0, 0.0),
+			new JSM.Coord (0.0, 0.0, 1.0)
+		);
 
-		this.viewer = new JSM.ThreeViewer ();
+		this.viewer = new JSM.Viewer ();
 		this.uiDiv = document.getElementById (uiDivName);
 		this.editor = new Editor ();
 
-		if (!this.viewer.Start (canvasName, viewerSettings)) {
+		if (!this.viewer.Init (canvasName, camera)) {
 			return false;
 		}
 
@@ -1038,12 +1038,11 @@ JSMDemo.prototype =
 	
 	AddBodyToViewer : function (body)
 	{
-		var meshes = JSM.ConvertBodyToThreeMeshes (body);
+		var geometries = JSM.ConvertBodyToRenderGeometries (body);
 		
-		this.viewer.RemoveMeshes ();
-		for (var i = 0; i < meshes.length; i++) {
-			this.viewer.AddMesh (meshes[i]);
-		}
+		this.viewer.RemoveGeometries ();
+		this.viewer.AddGeometries (geometries);
+		this.viewer.Draw ();
 	},
 	
 	Draw : function ()
