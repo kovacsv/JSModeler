f0bf862afebb765ce42979dc80cbc3b74d65218e
kovacsv <kowik1984@gmail.com>
2014-09-14 07:41:39 +0200

    Add MatrixDeterminant calculator function.

--- src/geometry/matrix.js
+++ src/geometry/matrix.js
@@ -192,6 +192,50 @@ JSM.MatrixMultiply = function (matrix1, matrix2)
 };
 
 /**
+* Function: MatrixDeterminant
+* Description: Calculates the determinant of a matrix.
+* Parameters:
+*	matrix {number[16]} the source matrix
+* Returns:
+*	{number} the determinant
+*/
+JSM.MatrixDeterminant = function (matrix)
+{
+	var a00 = matrix[0];
+	var a01 = matrix[1];
+	var a02 = matrix[2];
+	var a03 = matrix[3];
+	var a10 = matrix[4];
+	var a11 = matrix[5];
+	var a12 = matrix[6];
+	var a13 = matrix[7];
+	var a20 = matrix[8];
+	var a21 = matrix[9];
+	var a22 = matrix[10];
+	var a23 = matrix[11];
+	var a30 = matrix[12];
+	var a31 = matrix[13];
+	var a32 = matrix[14];
+	var a33 = matrix[15];
+
+	var b00 = a00 * a11 - a01 * a10;
+	var b01 = a00 * a12 - a02 * a10;
+	var b02 = a00 * a13 - a03 * a10;
+	var b03 = a01 * a12 - a02 * a11;
+	var b04 = a01 * a13 - a03 * a11;
+	var b05 = a02 * a13 - a03 * a12;
+	var b06 = a20 * a31 - a21 * a30;
+	var b07 = a20 * a32 - a22 * a30;
+	var b08 = a20 * a33 - a23 * a30;
+	var b09 = a21 * a32 - a22 * a31;
+	var b10 = a21 * a33 - a23 * a31;
+	var b11 = a22 * a33 - a23 * a32;
+	
+	var determinant = b00 * b11 - b01 * b10 + b02 * b09 + b03 * b08 - b04 * b07 + b05 * b06;
+	return determinant;
+};
+
+/**
 * Function: MatrixInvert
 * Description: Inverts a matrix.
 * Parameters:
