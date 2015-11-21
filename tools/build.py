436ee9726fafb64f85edce20e6b5f219b43ab954
kovacsv <kowik1984@gmail.com>
2015-11-21 16:29:54 +0100

    Update build system.

--- tools/build.py
+++ tools/build.py
@@ -1,190 +1,53 @@
 import os
 import sys
-import re
+from jsmbuild import jsmbuild
 
-header = '/* JSModeler [mainVersion].[subVersion] - http://www.github.com/kovacsv/JSModeler */ ';
-versionFileName = '../src/core/jsm.js'
-filesFileName = 'files.txt'
+def PrintTitle (title):
+	print '-- ' + title + ' --'
 
-externsFileName = 'jsmodeler.externs.js'
-externsViewerFileName = 'jsmodeler.viewer.externs.js'
-mergedFileName = 'jsmodeler_merged.js'
-mergedViewerFileName = 'jsmodeler.viewer_merged.js'
-resultFileName = '../build/jsmodeler.js'
-resultViewerFileName = '../build/jsmodeler.viewer.js'
+def PrintMessage (message):
+	print message
 
-def PrintInfo (message):
-	print ('Info: ' + message)
+def PrintErrors (builder):
+	for error in builder.GetErrors ():
+		print error
 
-def PrintError (error):
-	print ('Error: ' + error)
+def Main ():
+	builder = jsmbuild.JSMBuilder ()
 
-def GetLinesFromFile (fileName):
-	lines = []
-	if (not os.path.exists (fileName)):
-		return lines
-	
-	inputFile = open (fileName, 'r')
-	for line in inputFile:
-		lines.append (line.rstrip ('\r\n'))
-	inputFile.close ()
-
-	return lines
-	
-def MergeFiles (inputFileNames, outputFileName):
-	for fileName in inputFileNames:
-		if (not os.path.exists (fileName)):
-			return False
-		
-	outputFile = open (outputFileName, 'w')
-	for fileName in inputFileNames:
-		inputFile = open (fileName, 'r')
-		for line in inputFile:
-			outputFile.write (line)
-		outputFile.write ('\n')
-		inputFile.close ()
-
-	outputFile.close ()
-	return True
-
-def CompileFile (inputFileName, externFileName, outputFileName):
-	result = os.system ('java -jar compiler/compiler.jar --language_in=ECMASCRIPT5_STRICT --warning_level=VERBOSE --jscomp_off=globalThis --jscomp_off=checkTypes --externs ' + externFileName + ' --js ' + inputFileName + ' --js_output_file ' + outputFileName)
-	if result != 0:
-		return False
-	return True
-
-def GetVersion (versionFileName):
-	version = [0, 0]
-	if (not os.path.exists (versionFileName)):
-		return version
-
-	inputFile = open (versionFileName, 'r')
-	content = inputFile.read ()
-	inputFile.close ()
-	
-	match = re.search (r'this.mainVersion.*?=.*?(?P<mainVersion>\d+)', content)
-	if not match:
-		return version
-	mainVersion = match.group ('mainVersion')
-
-	match = re.search (r'this.subVersion.*?=.*?(?P<subVersion>\d+)', content)
-	if not match:
-		return version
-	subVersion = match.group ('subVersion')
-	
-	version[0] = mainVersion
-	version[1] = subVersion
-	return version
-	
-def GetHeader (version, header):
-	currentHeader = header
-	currentHeader = currentHeader.replace ('[mainVersion]', version[0])
-	currentHeader = currentHeader.replace ('[subVersion]', version[1])
-	return currentHeader
-	
-def WriteHeader (fileName, header):
-	if (not os.path.exists (fileName)):
-		return False
-
-	inputFile = open (fileName, 'r')
-	content = inputFile.read ()
-	inputFile.close ()
-
-	outputFile = open (fileName, 'w')
-	outputFile.write (header)
-	outputFile.write (content)
-	outputFile.close ()
-	
-	return True
-	
-def DeleteFile (fileName):
-	if (not os.path.exists (fileName)):
-		return False
-
-	os.remove (fileName)
-	return True
-
-def Build (inputFileNames, mergedFilePath, resultFilePath, externsFilePath, keepMergedFile):
-	versionsPath = os.path.abspath (versionFileName)
-	version = GetVersion (versionsPath);
-	if version == [0, 0]:
-		PrintError ('Invalid version.');
-		return 1
-
-	succeeded = MergeFiles (inputFileNames, mergedFilePath)
-	if not succeeded:
-		PrintError ('Not existing file in file list.');
-		return 1
-
-	succeeded = CompileFile (mergedFilePath, externsFilePath, resultFilePath)
-	if not succeeded:
-		PrintError ('Compilation failed.');
-		DeleteFile (resultFilePath)
-		DeleteFile (mergedFilePath)
-		return 1
-	
-	currentHeader = GetHeader (version, header)
-	if len (currentHeader) == 0:
-		PrintError ('Invalid header.');
-		DeleteFile (resultFilePath)
-		DeleteFile (mergedFilePath)
-		return 1
-	
-	succeeded = WriteHeader (resultFilePath, currentHeader)
-	if not succeeded:
-		PrintError ('Write header failed.');
-		DeleteFile (resultFilePath)
-		DeleteFile (mergedFilePath)
-		return 1
-	
-	if not keepMergedFile:
-		succeeded = DeleteFile (mergedFilePath)
-		if not succeeded:
-			PrintError ('Delete failed.');
-			DeleteFile (resultFilePath)
-			DeleteFile (mergedFilePath)
-			return 1
-	
-	return 0
-			
-def Main (argv):
+	PrintTitle ('Init build system')
 	currentPath = os.path.dirname (os.path.abspath (__file__))
-	os.chdir (currentPath)
-
-	keepMergedFiles = False
-	for i in range (1, len (argv)):
-		if argv[i] == 'keepMergedFiles':
-			keepMergedFiles = True
-
-	filesFilePath = os.path.abspath (filesFileName)
-	PrintInfo ('Collect files to merge from <' + filesFilePath + '>.')
-	inputFileNames = GetLinesFromFile (filesFilePath);
-	if len (inputFileNames) == 0:
-		PrintError ('Invalid file list.');
+	if not builder.Init (currentPath):
+		PrintErrors (builder)
 		return 1
-
-	coreFiles = []
-	viewerFiles = []
-	for fileName in inputFileNames:
-		if '../src/three' in fileName:
-			viewerFiles.append (fileName)
-		else:
-			coreFiles.append (fileName)
-
-	mergedFilePath = os.path.abspath (mergedFileName)
-	resultFilePath = os.path.abspath (resultFileName)
-	externsFilePath = os.path.abspath (externsFileName)
-	PrintInfo ('Compile files to <' + resultFilePath + '>.')
-	if Build (coreFiles, mergedFilePath, resultFilePath, externsFilePath, keepMergedFiles) != 0:
+    
+	PrintTitle ('Check dependencies')
+	if not builder.CheckDependencies ():
+		PrintErrors (builder)
 		return 1
 	
-	mergedFilePath = os.path.abspath (mergedViewerFileName)
-	resultFilePath = os.path.abspath (resultViewerFileName)
-	externsFilePath = os.path.abspath (externsViewerFileName)
-	PrintInfo ('Compile files to <' + resultFilePath + '>.')
-	if Build (viewerFiles, mergedFilePath, resultFilePath, externsFilePath, keepMergedFiles) != 0:
+	PrintTitle ('Run unit tests')
+	if not builder.RunUnitTests ():
+		PrintErrors (builder)
 		return 1
-		
+    
+	PrintTitle ('JSHint check')
+	if not builder.JSHintCheck ():
+		PrintErrors (builder)
+		return 1
+
+	PrintTitle ('Build')
+	if not builder.Build ():
+		PrintErrors (builder)
+		return 1
+	
+	PrintTitle ('Document')
+	if not builder.Document ():
+		PrintErrors (builder)
+		return 1
+
+	PrintMessage ('---------------------------')
+	PrintMessage ('Build finished successfully')
 	return 0
 	
-sys.exit (Main (sys.argv))
+sys.exit (Main ())
---
+++ tools/jsmbuild/jsmbuild.py
@@ -0,0 +1,189 @@
+import os
+import re
+import json
+from jsmdoc import jsmdoc
+
+def GetFileContent (fileName):
+	file = open (fileName, 'rb')
+	content = file.read ()
+	file.close ()
+	return content
+
+def WriteContentToFile (fileName, content):
+	file = open (fileName, 'wb')
+	file.write (content)
+	file.close ()
+
+def WriteHeaderToFile (fileName, header):
+	content = GetFileContent (fileName)
+	WriteContentToFile (fileName, header + content)
+	
+def MergeFiles (inputFileNames, outputFileName):
+	for fileName in inputFileNames:
+		if not os.path.exists (fileName):
+			return False
+	outputFile = open (outputFileName, 'wb')
+	for fileName in inputFileNames:
+		outputFile.write (GetFileContent (fileName))
+		outputFile.write ('\n')
+	outputFile.close ()
+	return True	
+
+def DeleteFile (fileName):
+	if os.path.exists (fileName):
+		os.remove (fileName)
+
+def LoadJsonFile (filePath):
+	jsonFile = open (filePath)
+	jsonContent = json.load (jsonFile)
+	jsonFile.close ()
+	return jsonContent
+		
+def CompileFile (inputFileName, externFileName, outputFileName):
+	result = os.system ('java -jar compiler/compiler.jar --language_in=ECMASCRIPT5_STRICT --warning_level=VERBOSE --jscomp_off=globalThis --jscomp_off=checkTypes --externs ' + externFileName + ' --js ' + inputFileName + ' --js_output_file ' + outputFileName)
+	if result != 0:
+		return False
+	return True
+
+class JSMBuilder:
+	def __init__ (self):
+		self.rootPath = None
+		self.files = None
+		self.errors = None
+
+	def Init (self, rootPath):
+		os.chdir (rootPath)
+		self.rootPath = rootPath
+		filesJsonPath = os.path.join (self.rootPath, 'files.json')
+		self.files = LoadJsonFile (filesJsonPath)
+		self.errors = []
+		return True
+
+	def GetErrors (self):
+		return self.errors
+	
+	def CheckDependencies (self):
+		def CheckUsedSymbols (inputFileName, definedSymbols, errors):
+			fileContent = GetFileContent (inputFileName)
+			jsmSymbolRegExp = 'JSM\.[a-zA-Z0-9\.]*'
+			
+			currentlyDefinedSymbols = []
+			defineStrings = re.findall ('(' + jsmSymbolRegExp + ')\s*=\s*function\s*\(', fileContent)
+			for defineString in defineStrings:
+				if not 'prototype' in defineString:
+					currentlyDefinedSymbols.append (defineString)
+			defineStrings = re.findall ('(' + jsmSymbolRegExp + ')\s*=\s*' + jsmSymbolRegExp, fileContent)
+			for defineString in defineStrings:
+				currentlyDefinedSymbols.append (defineString)
+		
+			everySymbolValid = True
+			for currentlyDefinedSymbol in currentlyDefinedSymbols:
+				if currentlyDefinedSymbol in definedSymbols:
+					errors.append ('Multiple defined symbol "' + currentlyDefinedSymbol + '" in file "' + inputFileName + '".')
+					everySymbolValid = False
+				else:
+					definedSymbols.append (currentlyDefinedSymbol)
+			useStrings = re.findall ('(' + jsmSymbolRegExp + ')\s*\(', fileContent)
+			for useString in useStrings:
+				if not useString in definedSymbols:
+					errors.append ('Invalid dependency "' + useString + '" in file "' + inputFileName + '".')
+					everySymbolValid = False
+			return everySymbolValid	
+		definedSymbols = []
+		success = True
+		for inputFileName in self.files['coreFileList']:
+			if not CheckUsedSymbols (inputFileName, definedSymbols, self.errors):
+				success = False
+		return success
+	
+	def RunUnitTests (self):
+		unitTestPath = os.path.join (self.rootPath, '..', 'test', 'unittest', 'jsmodelertest.js')
+		result = os.system ('sutest ' + unitTestPath + ' -silent')
+		if result != 0:
+			self.errors.append ('Unit tests failed.')
+			return False
+		return True
+	
+	def JSHintCheck (self):
+		configFilePath = os.path.join (self.rootPath, 'jshintconfig.json')
+		sourcesFolderName = os.path.join (self.rootPath, '..', 'src')
+		result = os.system ('jshint --config ' + configFilePath + ' ' + sourcesFolderName)
+		if result != 0:
+			self.errors.append ('Found JSHint errors.')
+			return False
+		return True
+	
+	def Build (self):
+		def GetVersion (versionFileName):
+			if not os.path.exists (versionFileName):
+				return None
+			content = GetFileContent (versionFileName)
+			match = re.search (r'this.mainVersion.*?=.*?(?P<mainVersion>\d+)', content)
+			if not match:
+				return None
+			mainVersion = match.group ('mainVersion')
+			match = re.search (r'this.subVersion.*?=.*?(?P<subVersion>\d+)', content)
+			if not match:
+				return None
+			subVersion = match.group ('subVersion')
+			return [mainVersion, subVersion]
+
+		def WriteVersionHeader (fileName, version):
+			header = '/* JSModeler [mainVersion].[subVersion] - http://www.github.com/kovacsv/JSModeler */ '
+			header = header.replace ('[mainVersion]', version[0])
+			header = header.replace ('[subVersion]', version[1])
+			WriteHeaderToFile (fileName, header)
+
+		def MergeAndCompileFiles (inputFileNames, mergedFilePath, externsFilePath, resultFilePath, version, errors):
+			if not MergeFiles (inputFileNames, mergedFilePath):
+				errors.append ('Failed to merge files to ' + mergedFilePath)
+				return False
+			if not CompileFile (mergedFilePath, externsFilePath, resultFilePath):
+				errors.append ('Failed to compile file to ' + resultFilePath)
+				DeleteFile (mergedFilePath)
+				return False
+			WriteVersionHeader (resultFilePath, version)
+			DeleteFile (mergedFilePath)
+			return True
+			
+		version = GetVersion (self.files['coreFileList'][0])
+		if version == None:
+			self.errors.append ('Invalid version.')
+			return False
+			
+		inputFileNames = self.files['coreFileList']
+		mergedFilePath = os.path.join (self.rootPath, 'jsmodeler.merged.js')
+		externsFilePath = os.path.join (self.rootPath, 'jsmodeler.externs.js')
+		resultFilePath = os.path.join (self.rootPath, '..', 'build', 'jsmodeler.js')
+		if not MergeAndCompileFiles (inputFileNames, mergedFilePath, externsFilePath, resultFilePath, version, self.errors):
+			return False
+			
+		inputFileNames = self.files['threeExtensionFileList']
+		mergedFilePath = os.path.join (self.rootPath, 'jsmodeler.viewer.merged.js')
+		externsFilePath = os.path.join (self.rootPath, 'jsmodeler.viewer.externs.js')
+		resultFilePath = os.path.join (self.rootPath, '..', 'build', 'jsmodeler.viewer.js')
+		if not MergeAndCompileFiles (inputFileNames, mergedFilePath, externsFilePath, resultFilePath, version, self.errors):
+			return False
+
+		return True
+
+	def Document (self):
+		projectName = 'JSModeler'
+		resultFilePath = os.path.join (self.rootPath, '..', 'documentation', 'jsmdoc', 'include', 'jsmdoc.json')
+		moduleNames = []
+		filesByModule = {}
+		for fileName in self.files['coreFileList']:
+			absPath = os.path.abspath (fileName)
+			moduleName = os.path.split (absPath)[0]
+			moduleName = os.path.split (moduleName)[1]
+			if not moduleName in moduleNames:
+				moduleNames.append (moduleName)
+			if not moduleName in filesByModule.keys ():
+				filesByModule[moduleName] = []
+			filesByModule[moduleName].append (absPath)
+		documentation = jsmdoc.Documentation (projectName)
+		for moduleName in moduleNames:
+			newModuleName = moduleName.title ()
+			documentation.AddModule (newModuleName, filesByModule[moduleName])
+		documentation.WriteJSON (resultFilePath)
+		return True
