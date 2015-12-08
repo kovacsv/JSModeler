db7967a928fc1b0c4489cd9ac763a739a1b45801
kovacsv <kowik1984@gmail.com>
2015-12-08 19:04:54 +0100

    Shorter output from renderertest.

--- test/renderertest/include/canvastesterapp.js
+++ test/renderertest/include/canvastesterapp.js
@@ -2,6 +2,9 @@ CanvasTesterApp = function (canvas, tolerance, resultsDivName)
 {
 	this.resultsDiv = document.getElementById (resultsDivName);
 	this.canvasTester = new CanvasTester (canvas, tolerance, this.TestFinished.bind (this), this.AllTestsFinished.bind (this));
+	this.testResultDiv = document.createElement ('div');
+	this.testResultDiv.className = 'testresult';
+	this.resultsDiv.appendChild (this.testResultDiv);
 };
 
 CanvasTesterApp.prototype.AddTest = function (renderCallback, referenceImage)
@@ -34,15 +37,13 @@ CanvasTesterApp.prototype.TestFinished = function (result, testObject, resultIma
 		parentDiv.appendChild (canvas);		
 	}
 	
-	function AddResultLine (result, testObject, resultImageData, referenceImageData, differenceImageData, parentDiv)
+	function AddResultLine (result, testObject, resultImageData, referenceImageData, differenceImageData, resultDiv, testResultDiv)
 	{
-		var resultDiv = document.createElement ('div');
-		
 		var resultTitle = document.createElement ('div');
 		var success = (result == 0);
 		var errorText = '';
 		if (result === 0) {
-			errorText = 'success';
+			errorText = 'ok';
 		} else if (result === 1) {
 			errorText = 'missing reference';
 		} else if (result === 2) {
@@ -51,35 +52,36 @@ CanvasTesterApp.prototype.TestFinished = function (result, testObject, resultIma
 			errorText = 'difference';
 		}
 		resultTitle.className = 'resulttitle ' + (success ? 'success' : 'failure');
-		resultTitle.innerHTML = testObject.referenceImage + ' - ' + errorText;
+		resultTitle.innerHTML = errorText;
 		resultDiv.appendChild (resultTitle);
 		
-		var resultImages = document.createElement ('div');
-		var resultImagesGenerated = false;
-		resultImages.className = 'resultimages';
-		resultDiv.appendChild (resultImages);
-
 		resultTitle.onclick = function () {
-			if (!resultImagesGenerated) {
-				DrawImageData (resultImageData, resultImages);
-				if (!success && referenceImageData !== null) {
-					DrawImageData (referenceImageData, resultImages);
-				}
-				if (!success && differenceImageData !== null) {
-					DrawImageData (differenceImageData, resultImages);
-				}
-				resultImagesGenerated = true;
+			while (testResultDiv.lastChild) {
+				testResultDiv.removeChild (testResultDiv.lastChild);
 			}
-			if (resultImages.style.display != 'block') {
-				resultImages.style.display = 'block';
-				testObject.renderCallback (function () {});
-			} else {
-				resultImages.style.display = 'none';
+			var titleDiv = document.createElement ('div');
+			titleDiv.className = 'testresulttitle';
+			titleDiv.innerHTML = testObject.referenceImage;
+			testResultDiv.appendChild (titleDiv);
+			
+			var mainDiv = document.createElement ('div');
+			mainDiv.className = 'testresultmain';
+			DrawImageData (resultImageData, mainDiv);
+			if (!success && referenceImageData !== null) {
+				DrawImageData (referenceImageData, mainDiv);
 			}
+			if (!success && differenceImageData !== null) {
+				DrawImageData (differenceImageData, mainDiv);
+			}
+			testResultDiv.appendChild (mainDiv);
+			if (testResultDiv.style.display != 'block') {
+				testResultDiv.style.display = 'block';
+			}
+			testObject.renderCallback (function () {});
 		};
 		
-		parentDiv.appendChild (resultDiv);
+		resultDiv.appendChild (resultTitle);
 	}
 
-	AddResultLine (result, testObject, resultImageData, referenceImageData, differenceImageData, this.resultsDiv);
+	AddResultLine (result, testObject, resultImageData, referenceImageData, differenceImageData, this.resultsDiv, this.testResultDiv);
 };
