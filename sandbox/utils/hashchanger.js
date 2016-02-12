0d1d7ef3d1d53d28d5160c8df5fe364578a45bca
kovacsv <kowik1984@gmail.com>
2016-02-12 20:00:29 +0100

    Use hash indices in pushpull example.

---
+++ sandbox/utils/hashchanger.js
@@ -0,0 +1,37 @@
+HashChanger = function (hashChangedCallback)
+{
+	this.hashChangedCallback = hashChangedCallback;
+	window.onhashchange = this.OnHashChange.bind (this);
+	window.onkeydown = this.OnKeyDown.bind (this);
+};
+
+HashChanger.prototype.GetHashIndex = function ()
+{
+	var hashIndex = 0;
+	var hashString = window.location.hash;
+	if (hashString.length > 0) {
+		hashIndex = parseInt (hashString.substr (1), 10);
+	}
+	if (isNaN (hashIndex)) {
+		hashIndex = 0;
+	}
+	return hashIndex;
+};
+
+HashChanger.prototype.OnHashChange = function (event)
+{
+	this.hashChangedCallback ();
+};
+
+HashChanger.prototype.OnKeyDown = function (event)
+{
+	var keyCode = event.which;
+	var hash = this.GetHashIndex ();
+	if (keyCode == 37) {
+		hash--;
+	} else if (keyCode == 39) {
+		hash++;
+	}
+	hashString = hash.toString ();
+	window.location.hash = hashString;	
+};
