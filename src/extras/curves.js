abaded48e4ba3fcb7a316c6d069403c5d5f777e5
kovacsv <kowik1984@gmail.com>
2016-10-04 19:00:59 +0200

    PathPolygon tryout.

--- src/extras/curves.js
+++ src/extras/curves.js
@@ -27,6 +27,42 @@ JSM.BernsteinPolynomial = function (k, n, x)
 };
 
 /**
+* Function: GenerateCubicBezierCurve
+* Description: Generates a bezier curve from the given points.
+* Parameters:
+*	p0 {Coord2D} point 1
+*	p1 {Coord2D} point 2
+*	p2 {Coord2D} point 3
+*	p3 {Coord2D} point 4
+*	segmentation {integer} the segmentation of the result curve
+* Returns:
+*	{Coord2D[]} the result
+*/
+JSM.GenerateCubicBezierCurve = function (p0, p1, p2, p3, segmentation)
+{
+	function GetCubicBezierPoint (p0, p1, p2, p3, t)
+	{
+		var t2 = t * t;
+		var t3 = t2 * t;
+		var invT = 1.0 - t;
+		var invT2 = invT * invT;
+		var invT3 = invT2 * invT;
+		var x = invT3 * p0.x + 3.0 * invT2 * t * p1.x  + 3.0 * invT * t2 * p2.x + t3 * p3.x;
+		var y = invT3 * p0.y + 3.0 * invT2 * t * p1.y  + 3.0 * invT * t2 * p2.y + t3 * p3.y;
+		return new JSM.Coord2D (x, y);
+	}
+	
+	var result = [];
+	var s = 1.0 / segmentation;
+	var i, coord;
+	for (i = 0; i <= segmentation; i++) {
+		coord = GetCubicBezierPoint (p0, p1, p2, p3, i * s);
+		result.push (coord);
+	}
+	return result;
+};
+
+/**
 * Function: GenerateBezierCurve
 * Description: Generates a bezier curve from the given points.
 * Parameters:
